d2c4844dd4eb8a7de9447df44e2503ec68f96d0d
maddenlockin <maddenlockin@gmail.com>
2021-02-11 13:04:43 -0800

    colorful calculator

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
 // set event listeners to update state and DOM
 // set listeners on each button, do math, and update DOM
 
-const addButton = document.getElementById('addition-sum');
+const addButton = document.getElementById('addition-answer');
 const addInputOne = document.getElementById('addition-input-one');
 const addDisplay = document.getElementById('addition-display');
 const addInputTwo = document.getElementById('addition-input-two');
 
 addButton.addEventListener('click', () => {
-    addDisplay.value = Number(addInputOne.value) + Number(addInputTwo.value);
+    addDisplay.textContent = Number(addInputOne.value) + Number(addInputTwo.value);
 });
 
 const subButton = document.getElementById('subtraction-answer');
@@ -20,5 +20,22 @@ const subDisplay = document.getElementById('subtraction-display');
 const subInputTwo = document.getElementById('subtraction-input-two');
 
 subButton.addEventListener('click', () => {
-    subDisplay.textcontent = Number(subInputOne.value) - Number(subInputTwo.value);
+    subDisplay.textContent = Number(subInputOne.value) - Number(subInputTwo.value);
+});
+
+const multiplyButton = document.getElementById('multiplication-answer');
+const multiplyInputOne = document.getElementById('multiplication-input-one');
+const multiplyDisplay = document.getElementById('multiplication-display');
+const multiplyInputTwo = document.getElementById('multiplication-input-two');
+
+multiplyButton.addEventListener('click', () => {
+    multiplyDisplay.textContent = Number(multiplyInputOne.value) * Number(multiplyInputTwo.value);
+});
+const divideButton = document.getElementById('division-answer');
+const divideInputOne = document.getElementById('division-input-one');
+const divideDisplay = document.getElementById('division-display');
+const divideInputTwo = document.getElementById('division-input-two');
+
+divideButton.addEventListener('click', () => {
+    divideDisplay.textContent = Number(divideInputOne.value) / Number(divideInputTwo.value);
 });
